2760e4dca4a50781f59a50af92f3f18ece8064d6
cirellabella <Cirellasmith@gmail.com>
2023-04-05 19:59:06 -0700

    Initial commit

---
+++ Bicycle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_1
+{
+    public class Bicycle
+    {
+        //Fields
+        string _brakes;
+        string _chain;
+        string _typesOfWheels;
+        int _numberOfGears;
+        //frame
+        string _pedals;
+        string _handle;
+        int _numberOfSeats;
+
+        //Constructor
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Week_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bicycle bicycle= new Bicycle();
+
+            Console.WriteLine(bicycle.);
+
+
+        }//Main
+    }//class
+}//namespace
